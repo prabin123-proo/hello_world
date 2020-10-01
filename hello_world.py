ae3951a29682eef06ff858a364d92c80e974a9c8
prabin123-proo <71684870+prabin123-proo@users.noreply.github.com>
2020-10-01 20:52:05 +0545

    BE CREATIVE MAKE CREATIVE

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print "Hello World!"
+print("hh       hh   eeeeeeeeee  l                   l                oooooooooooooo      Wwwwwwww          ")
+print("hh       hh   eeeeeeee    ll                 ll                oo          oo      Ooooooooooooooooo ")
+print("hh       hh   ee          lll               lll                oo          oo      Rrrrrrrrrrrr")
+print("hh       hh   ee          lll               lll                oo          oo      Llllllll") 
+print("hh       hh   ee          lll               lll                oo          oo      Ddddd")
+print("hhhhhhhhhhh   eeeeeee     lll               lll                oo          oo          :)
+print("hh       hh   eeeeeee     lllllllllllllll   llllllllllllll      ooooooooooooo                                       ")
+print("hh       hh   eeeeeeeee   lllllllllllll     llllllllllll                                              ")
+
+    
